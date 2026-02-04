4bfacee1204cf055062251e0740edc62f37c36ce
Edy Epumuceno Rodrigues Junior <edyjrdev@gmail.com>
2026-02-03 21:17:59 -0300

    atualizado 002

---
+++ bcmp_python_fundamentos/002.py
@@ -0,0 +1,51 @@
+# Tipos de Dados
+# Tamanho em memória
+# Operações
+
+# Tipos Comuns
+
+# Numérico: int, float, complex
+# int()
+a = 1
+b = 2
+c = 3
+# float()
+d = 1.0
+e = 2.0
+f = 3.0
+# complex()
+g = 1j
+h = 2j
+i = 3j
+
+# Booleano: bool
+# bool()
+j = True
+k = False
+
+# Texto: str
+# str()
+frase = 'Ouviram do Ipiranga'
+poema = "O 'ser' ou não 'ser' é um questionamento"
+nome = "Edy Junior"
+sobrenome = 'Junior'
+paragrafo  = """
+Um dia lindo
+vou programar
+ate ficar feliz
+e o sono me derrubar."""
+
+# Sequencia: list, tuple, range
+# list()
+# tuple()
+# range()
+# Mapa: dict
+# dict()
+# Coleção: set, frozenset
+# set()
+# frozenset()
+# Binário: bytes, bytearray, memoryview
+# bytes()
+# bytearray()
+# memoryview()
+# None: NoneType
